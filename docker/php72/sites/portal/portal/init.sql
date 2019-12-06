151830c55b9b6a0730fed8253caf81528cda9db5
Nikolay Nikolaev <evrinoma@gmail.com>
2019-12-06 19:37:56 +0300

    deploy docker

---
+++ docker/php72/sites/portal/portal/init.sql
@@ -0,0 +1,28 @@
+#!/bin/bash
+pass=$1
+serverSql=$2
+base=$3
+user=$4
+basepass=$5
+INIT_SQL="/root/init.sql"
+SERVER_SQL="/root/content.sql"
+	echo "
+	CREATE DATABASE \`$base\`;
+	CREATE USER '$user'@'localhost' IDENTIFIED BY '$basepass';
+	GRANT ALL PRIVILEGES ON \`$user\` . * TO '$user'@'localhost';
+	CREATE USER '$user'@'172.18.0.0/255.255.0.0' IDENTIFIED BY '$basepass';
+	GRANT ALL PRIVILEGES ON \`$base\` . * TO '$user'@'172.18.0.0/255.255.0.0';
+	FLUSH PRIVILEGES;
+	" > $INIT_SQL
+	mariaInstall=`yum list installed | grep "mariadb\."`
+	if [ -z "$mariaInstall" ]; then 
+		yum install mysql -y
+		mysql -h$serverSql -uroot -p$pass  < $INIT_SQL
+		mysql -h$serverSql -uroot -p$pass $base < $SERVER_SQL
+		yum remove mysql -y
+	else
+		mysql -h$serverSql -uroot -p$pass  < $INIT_SQL
+		mysql -h$serverSql -uroot -p$pass $base < $SERVER_SQL
+	fi
+	rm -f $INIT_SQL
+	rm -f $SERVER_SQL
