1b92c7c7f615f1f2d1c86d23238b3b2f31c96c24
nikolns <0631>
2020-04-07 19:59:04 +0300

    update and versioning roundcube

--- docker/php72/sites/roundcubemail/roundcubemail/init.sql
+++ docker/php72/sites/roundcubemail/roundcubemail/init.sql
@@ -1,14 +1,15 @@
-#!/bin/bash
+ #!/bin/bash
 	pass=$1
 	serverSql=$2
 	DIR=$3
 	INIT_SQL="/tmp/init.sql"
 	cd $DIR
-	echo "
-	CREATE DATABASE \`roundcubemail\`;
-	CREATE USER 'roundcubemail'@'172.18.0.0/255.255.0.0' IDENTIFIED BY 'roundcubemail';
-	GRANT ALL PRIVILEGES ON \`roundcubemail\` . * TO 'roundcubemail'@'172.18.0.0/255.255.0.0';
-	" > $INIT_SQL
+	echo "CREATE DATABASE \`roundcubemail_VERSION\`;" > $INIT_SQL
+	user=$(mysql -h$serverSql -uroot -p$pass -e "SELECT user FROM mysql.user WHERE user = 'roundcubemail';" | grep user)
+    if [ -z $user ]; then
+        echo "CREATE USER 'roundcubemail'@'172.18.0.0/255.255.0.0' IDENTIFIED BY 'roundcubemail';" >> $INIT_SQL
+    fi
+    echo "GRANT ALL PRIVILEGES ON \`roundcubemail_VERSION\`. * TO 'roundcubemail'@'172.18.0.0/255.255.0.0';" >> $INIT_SQL
 	mysql -h$serverSql -uroot -p$pass  < $INIT_SQL
-	mysql -h$serverSql -uroot -p$pass roundcubemail < /opt/WWW/container.ite-ng.ru/projects/roundcubemail/SQL/mysql.initial.sql
-	rm -f $INIT_SQL
+	mysql -h$serverSql -uroot -p$pass roundcubemail_VERSION < /opt/WWW/container.ite-ng.ru/projects/roundcubemail/VERSION/SQL/mysql.initial.sql
+	#rm -f $INIT_SQL
