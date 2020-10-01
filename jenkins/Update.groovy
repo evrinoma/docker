189f9b840403e0c8590d5abee7b221bbff11d376
evrinoma <evrinoma@gmail.com>
2020-10-01 14:52:29 +0300

    fix groovy meeting deploy

--- jenkins/Update.groovy
+++ jenkins/Update.groovy
@@ -1,6 +1,19 @@
 node {
     def mailRecipients = "nikolns@ite-ng.ru"
     try {
+        stage('Build Meeting Docker') {
+            def dockerName = 'meeting'
+            def dockerRootDir = '/opt/docker'
+            def dockerScriptDir = "${dockerRootDir}/scripts"
+            def remote = [:]
+            remote.name = dockerName
+            remote.host = '172.16.45.4'
+            remote.user = 'user'
+            remote.port = 749
+            remote.password = 'pass'
+            remote.allowAnyHosts = true
+            sshCommand remote: remote, command: "echo -e \"${remote.password}\" | sudo -S bash -c \"cd ${dockerScriptDir} && ./build.prod\""
+        }
         stage('Build SFTP Docker') {
             def dockerName = 'sftp'
             def dockerRootDir = '/opt/docker'
